275fb0a5c9a3c73255663bba970733eb9f8f97e6
Koishi32 <alejandroivanleyvadiaz@gmail.com>
2020-01-31 13:14:13 -0600

    Zombie_Move
    The zombies now move and the raycast has been implemented

--- udemy_course/Assets/Scripts/GameMa.cs
+++ udemy_course/Assets/Scripts/GameMa.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class GameMa : MonoBehaviour
 {
+    public GameObject[] zombies;
+    bool isRaising = false;
+    bool isFailing = false;
+    int AcctiveZombieIndex = 0;
+    Vector2 startPosition;
+    [SerializeField]
+    float riseSpeed;
+    [SerializeField]
+    float top_limit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickNewZombie();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isRaising)
+        {
+            if (zombies[AcctiveZombieIndex].transform.position.y - startPosition.y >= top_limit)
+            {
+                isRaising = false;
+                isFailing = true;
+            }
+            else {
+                zombies[AcctiveZombieIndex].transform.Translate(Vector2.up * Time.deltaTime * riseSpeed);
+            }
+        }
+        else if (isFailing)
+        {
+            if (zombies[AcctiveZombieIndex].transform.position.y - startPosition.y <= 0)
+            {
+                isRaising = false;
+                isFailing = false;
+            }
+            else {
+                zombies[AcctiveZombieIndex].transform.Translate(Vector2.down * Time.deltaTime * riseSpeed);
+            }
+        }
+        else {
+            zombies[AcctiveZombieIndex].transform.position = startPosition;
+            pickNewZombie();
+        }
         
+    }
+
+    private void pickNewZombie() {
+        isRaising = true;
+        isFailing = false;
+        AcctiveZombieIndex = Random.Range(0, zombies.Length);
+        startPosition = zombies[AcctiveZombieIndex].transform.position;
+    
     }
 }
---
+++ udemy_course/Assets/Scripts/Playerinput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerinput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit.collider != null) {
+                if (hit.collider.tag == "Zombie")
+                {
+                    Debug.Log(hit.collider.name);
+                }
+            }
+        }
+        
+    }
+}
